15959756dbe06b8e61e8412b25ebf163a45d035f
Ghaida Omar <ghaidaomar@MacBook-Pro-alkhas-b-Ghaida.local>
2020-05-28 05:08:15 +0300

    first commit

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 $(document).ready(function () { 
-   // Creates the grid upon clicking the button Submit
+    // ينشاءالجدول عند النقر فوق الزر إرسال
   $('#SizePicker').submit(function makeGrid(grid) { 
-    // Lets the grid be cleared when hitting the Submit button again
+    // يتم مسح الجدول عند الضغط على زر الارسال 
     $('table tr').remove(); 
-    // Allows the user to add a chosen value inside the input box to add rows 
+    // يسمح للمستخدم باضافة قيمة اختيارية داخل مربع الإدخال لإضافة صفوف
     var row_input = $('#Input_Height').val(); 
-    // Allows the user to add a chosen value inside the 2nd input box to add columns
-    var col_input = $('#Input_Width').val(); 
+    // يسمح للمستخدم بإضافة قيمة اختيارية داخل مربع الإدخال الثاني لإضافة أعمدة
+    var col_input = $('#Input_Width').val();
+    // يقوم الحلقة بإنشاء صف من الخلايا 
     for (var i = 1; i <= row_input; i++) { 
-      // This loop creates a row of cells
-      $('table').append("<tr></tr>"); 
+      $('table').append("<tr></tr>");
+   // تضيف هذي الحلقة خلية بعد كل صف 
       for (var j = 1; j <= col_input; j++) {
-         // This loop adds a cell after every row
         $('tr:last').append("<td></td>");
-        // For every 'td', a class of 'cells' is created
         $('td').attr("class", 'cells') 
       }
     }
-    // Prevents the grid to be deleted after it is created
-
+    // يمنع الشبكة من حذفها بعد إنشائها بالصفحة
     grid.preventDefault(); 
-    // The function allows the user to color a cell on click
+   // الوظيفة تسمح للمستخدم تلوين الخلية عند الضغط عليها
     $('.cells').click(function (event) { 
-      var paint = $('#ColorPicker').val();
-       // Lets the chosen color on a click event to be added to the grid
+      var paint = $('#ColorTool').val();
+    // يسمح  اللون الذي تم اختياره في الحدث الضغط لإضافته إلى الشبكة
       $(event.target).css('background-color', paint);
     });
   }); 
